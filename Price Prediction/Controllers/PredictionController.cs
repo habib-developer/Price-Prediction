79f455d3eb10bd7004fc33c6813b3d2d4e4ed66d
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-08-25 22:22:08 +0500

    Add project files.

---
+++ Price Prediction/Controllers/PredictionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using PricePredictionML.Model.DataModels;
+
+namespace Price_Prediction.Controllers
+{
+    public class PredictionController : Controller
+    {
+        public IActionResult Price(ModelInput input)
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+            // Create predection engine related to the loaded train model
+            ITransformer mlModel = mlContext.Model.Load(@"..\Price PredictionML.Model\MLModel.zip", out var modelInputSchema);
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Try model on sample data to predict fair price
+            ModelOutput result = predEngine.Predict(input);
+
+            ViewBag.Price = result.Score;
+            return View(input);
+        }
+    }
+}
